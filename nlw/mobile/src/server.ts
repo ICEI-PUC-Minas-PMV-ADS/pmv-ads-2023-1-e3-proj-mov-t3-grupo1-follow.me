8671a04625fd824f7731dcb4025f0044c9aff1e8
katrynribeiro <kakarilber@hotmail.com>
2023-04-23 12:02:22 -0300

    configurando mobile

---
+++ nlw/mobile/src/server.ts
@@ -0,0 +1,26 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { Prisma, PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/hello', async () => {
+    const habits = await prisma.atividade.findMany({
+        where: {
+            nome: {
+                startsWith: 'Beber'
+        }
+    }
+ })
+
+    return habits
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP Server running!')
+})
