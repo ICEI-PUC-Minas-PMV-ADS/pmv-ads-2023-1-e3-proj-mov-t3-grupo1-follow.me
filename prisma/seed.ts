5096b700abb79baf73408d6029cd7a2bd8b3b055
katrynribeiro <kakarilber@hotmail.com>
2023-04-28 20:38:48 -0300

    convertendo banco em inglês

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,65 +2,67 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const primeiraAtividadeId = '0730ffac-d039-4194-9571-01aa2aa0efbd'
-const primeiraAtividadeCreationDate = new Date('2022-12-31T03:00:00.000')
+const firstHabitId = '0730ffac-d039-4194-9571-01aa2aa0efbd'
+const firstHabitCreationDate = new Date('2022-12-31T03:00:00.000')
 
-const segundaAtividadeId = '00880d75-a933-4fef-94ab-e05744435297'
-const segundaAtividadeCreationDate = new Date('2023-01-03T03:00:00.000')
+const secondHabitId = '00880d75-a933-4fef-94ab-e05744435297'
+const secondHabitCreationDate = new Date('2023-01-03T03:00:00.000')
 
-const terceiraAtividadeId = 'fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00'
-const terceiraAtividadeCreationDate = new Date('2023-01-08T03:00:00.000')
+const thirdHabitId = 'fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00'
+const thirdHabitCreationDate = new Date('2023-01-08T03:00:00.000')
 
 async function run() {
-  await prisma.atividade.deleteMany()
-  await prisma.dia.deleteMany()
+  await prisma.dayHabit.deleteMany();
+  await prisma.habitWeekDays.deleteMany();
+  await prisma.day.deleteMany();
+  await prisma.habit.deleteMany();
 
   /**
    * Create habits
    */
   await Promise.all([
-    prisma.atividade.create({
+    prisma.habit.create({
       data: {
-        id: primeiraAtividadeId,
-        nome: 'Beber 2L água',
-        data_criacao: primeiraAtividadeCreationDate,
-        DiaSemanaAtividade: {
+        id: firstHabitId,
+        title: 'Beber 2L água',
+        created_at: firstHabitCreationDate,
+        weekDays: {
           create: [
-            { dia_semana: 1 },
-            { dia_semana: 2 },
-            { dia_semana: 3 },
+            { week_day: 1 },
+            { week_day: 2 },
+            { week_day: 3 },
           ]
         }
       }
     }),
 
-    prisma.atividade.create({
+    prisma.habit.create({
       data: {
-        id: segundaAtividadeId,
-        nome: 'Exercitar',
-        data_criacao: segundaAtividadeCreationDate,
-        DiaSemanaAtividade: {
+        id: secondHabitId,
+        title: 'Exercitar',
+        created_at: secondHabitCreationDate,
+        weekDays: {
           create: [
-            { dia_semana: 3 },
-            { dia_semana: 4 },
-            { dia_semana: 5 },
+            { week_day: 3 },
+            { week_day: 4 },
+            { week_day: 5 },
           ]
         }
       }
     }),
 
-    prisma.atividade.create({
+    prisma.habit.create({
       data: {
-        id: terceiraAtividadeId,
-        nome: 'Dormir 8h',
-        data_criacao: terceiraAtividadeCreationDate,
-        DiaSemanaAtividade: {
+        id: thirdHabitId,
+        title: 'Dormir 8h',
+        created_at: thirdHabitCreationDate,
+        weekDays: {
           create: [
-            { dia_semana: 1 },
-            { dia_semana: 2 },
-            { dia_semana: 3 },
-            { dia_semana: 4 },
-            { dia_semana: 5 }
+            { week_day: 1 },
+            { week_day: 2 },
+            { week_day: 3 },
+            { week_day: 4 },
+            { week_day: 5 },
           ]
         }
       }
@@ -71,13 +73,13 @@ async function run() {
     /**
      * Habits (Complete/Available): 1/1
      */
-    prisma.dia.create({
+    prisma.day.create({
       data: {
         /** Monday */
-        data: new Date('2023-01-02T03:00:00.000z'),
-        DiaAtividade: {
+        date: new Date('2023-01-02T03:00:00.000z'),
+        dayHabits: {
           create: {
-            id_atividade: primeiraAtividadeId,
+            habit_id: firstHabitId,
           }
         }
       }
@@ -86,13 +88,13 @@ async function run() {
     /**
      * Habits (Complete/Available): 1/1
      */
-    prisma.dia.create({
+    prisma.day.create({
       data: {
         /** Friday */
-        data: new Date('2023-01-06T03:00:00.000z'),
-        DiaAtividade: {
+        date: new Date('2023-01-06T03:00:00.000z'),
+        dayHabits: {
           create: {
-            id_atividade: primeiraAtividadeId,
+            habit_id: firstHabitId,
           }
         }
       }
@@ -101,14 +103,14 @@ async function run() {
     /**
      * Habits (Complete/Available): 2/2
      */
-    prisma.dia.create({
+    prisma.day.create({
       data: {
         /** Wednesday */
-        data: new Date('2023-01-04T03:00:00.000z'),
-        DiaAtividade: {
+        date: new Date('2023-01-04T03:00:00.000z'),
+        dayHabits: {
           create: [
-            { id_atividade: primeiraAtividadeId },
-            { id_atividade: segundaAtividadeId },
+            { habit_id: firstHabitId },
+            { habit_id: secondHabitId },
           ]
         }
       }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import Fastify from 'fastify'
-import cors from '@fastify/cors'
-import { appRoutes } from './lib/routes'
-const app = Fastify()
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import { appRoutes } from './lib/routes';
 
-app.register(cors)
+const app = Fastify();
+
+app.register(cors);
 app.register(appRoutes)
 
 app.listen({
-    port: 3333,
+  port: 3333,
+  host:"0.0.0.0"
 }).then(() => {
-    console.log('HTTP Server running!')
-})
+  console.log('Server running in port: 3333!')
+});
