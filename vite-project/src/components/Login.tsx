3b2888ac97765bd280ddc10b88f16ad895be70ac
paolamarsura <paolamarsura@gmail.com>
2023-05-07 22:55:50 -0300

    login, routes

--- vite-project/src/components/Login.tsx
+++ vite-project/src/components/Login.tsx
@@ -1,61 +1,49 @@
 import React from 'react';
 import * as Tabs from '@radix-ui/react-tabs';
-import '../styles/login.css'; 
+import '../styles/login.css';
+import { FormEvent, useState } from "react";
+import { api } from "../lib/axios";
 
-const TabsDemo = () => (
+
+const Login = () => {
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    api.post('login', {
+      username,
+      password
+    })
+    
+    console.log("submit", {username, password});
+  };
+
+  return (  
   <Tabs.Root className="TabsRoot" defaultValue="tab1">
     <Tabs.List className="TabsList" aria-label="Manage your account">
-      <Tabs.Trigger className="TabsTrigger" value="tab1">
-        Account
-      </Tabs.Trigger>
-      <Tabs.Trigger className="TabsTrigger" value="tab2">
-        Password
-      </Tabs.Trigger>
+      <Tabs.Trigger className="TabsTrigger" value="tab1">Account</Tabs.Trigger>
     </Tabs.List>
     <Tabs.Content className="TabsContent" value="tab1">
-      <p className="Text">Make changes to your account here. Click save when you're done.</p>
-      <fieldset className="Fieldset">
-        <label className="Label" htmlFor="name">
-          Name
-        </label>
-        <input className="Input" id="name" defaultValue="Pedro Duarte" />
-      </fieldset>
-      <fieldset className="Fieldset">
-        <label className="Label" htmlFor="username">
-          Username
-        </label>
-        <input className="Input" id="username" defaultValue="@peduarte" />
-      </fieldset>
-      <div style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}>
-        <button className="Button green">Save changes</button>
-      </div>
-    </Tabs.Content>
-    <Tabs.Content className="TabsContent" value="tab2">
-      <p className="Text">Change your password here. After saving, you'll be logged out.</p>
-      <fieldset className="Fieldset">
-        <label className="Label" htmlFor="currentPassword">
-          Current password
-        </label>
-        <input className="Input" id="currentPassword" type="password" />
-      </fieldset>
-      <fieldset className="Fieldset">
-        <label className="Label" htmlFor="newPassword">
-          New password
-        </label>
-        <input className="Input" id="newPassword" type="password" />
-      </fieldset>
-      <fieldset className="Fieldset">
-        <label className="Label" htmlFor="confirmPassword">
-          Confirm password
-        </label>
-        <input className="Input" id="confirmPassword" type="password" />
-      </fieldset>
-      <div style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}>
-        <button className="Button green">Change password</button>
-      </div>
+      <form className="form" onSubmit={handleSubmit}>
+        <p className="Text">Make changes to your account here. Click save when you're done.</p>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="username">Username</label>
+          <input className="Input" id="username" value={username} onChange={event => setUserName(event.target.value)}/>
+        </fieldset>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="passoword">Password</label>
+          <input className="Input" id="password" id="username" value={password} onChange={event => setPassword(event.target.value)}/>
+        </fieldset>
+        <div style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}>
+          <button className="Button green">Log-In</button>
+        </div>
+      </form>
     </Tabs.Content>
   </Tabs.Root>
-);
+  )
+  };
 
-export default TabsDemo;
+export default Login;
 
