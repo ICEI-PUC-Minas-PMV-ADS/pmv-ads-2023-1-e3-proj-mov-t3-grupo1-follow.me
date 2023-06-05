3b42575eabe9334e24cefa0e08b1deb77ce90e51
katrynribeiro <kakarilber@hotmail.com>
2023-06-05 17:41:56 -0300

    parte da implementação de notificações

--- nlw/mobile/App.tsx
+++ nlw/mobile/App.tsx
@@ -1,6 +1,10 @@
 import './src/lib/dayjs';
 
 import { StatusBar } from 'react-native';
+// import * as Device from "expo-device"; 
+import { Subscription } from "expo-modules-core"; 
+import * as Notifications from 'expo-notifications';
+
 import {
   useFonts,
   Inter_400Regular,
@@ -11,8 +15,34 @@ import {
 
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
+import { Button } from 'react-native-elements';
+
+Notifications.setNotificationHandler({
+  handleNotification:async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowAlert: true,
+  }),
+});
 
 export default function App() {
+
+  async function handleCallNotification() {
+  //  let token = (await Notifications.getDevicePushTokenAsync()).data;
+  //  console.log(token);
+
+    await Notifications.scheduleNotificationAsync({
+      content:{
+        title: 'Viva uma vida mais organizada!',
+        body: 'Cheque a sua rotina diária',
+        data:{},
+      },
+      trigger:{
+        seconds:5,
+      },
+    });
+  }
+
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
@@ -27,6 +57,7 @@ export default function App() {
   return (
     <>
       <Routes />
+      <Button title='Notificações'onPress={handleCallNotification}/>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
