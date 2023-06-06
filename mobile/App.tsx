bacf0706aeeacd375aaa62267fb3c19767e95a08
katrynribeiro <kakarilber@hotmail.com>
2023-06-06 09:47:24 -0300

    finalização das notificações

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,5 @@
 import './src/lib/dayjs';
-
 import { StatusBar } from 'react-native';
-// import * as Device from "expo-device"; 
 import { Subscription } from "expo-modules-core"; 
 import * as Notifications from 'expo-notifications';
 
@@ -29,8 +27,6 @@ Notifications.setNotificationHandler({
 export default function App() {
 
   async function handleCallNotification() {
-  //  let token = (await Notifications.getDevicePushTokenAsync()).data;
-  //  console.log(token);
 
     await Notifications.scheduleNotificationAsync({
       content:{
